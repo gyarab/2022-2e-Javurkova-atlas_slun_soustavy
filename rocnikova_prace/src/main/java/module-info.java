108f48fd7baa8cdfadd6e2bf055052afef825815
sabcajavurkova <93973914+sabcajavurkova@users.noreply.github.com>
2023-04-25 21:48:00 +0200

    Delete rocnikova_prace directory

--- rocnikova_prace/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.rocnikova_prace {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.rocnikova_prace to javafx.fxml;
-    exports com.example.rocnikova_prace;
-}
